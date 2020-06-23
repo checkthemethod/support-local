f075ec2c53bbd0908f36515c0f33420596f93af0
Richard Saethang <richard.saethang@gmail.com>
2020-06-22 23:29:42 -0700

    Refactors layout of grid

--- src/components/SubGridView.js
+++ src/components/SubGridView.js
@@ -10,7 +10,7 @@ const SubGridView = ({ restaurantItems, searchTerm, currentFilter }) => (
         <div key={i} className="col-md-4 mb-4">
           <div className="card">
             <div className="card-body">
-              <h6 className="card-title">
+              <h4 className="card-title">
                 <a
                   href={`${x.website}`}
                   target="_blank"
@@ -18,31 +18,45 @@ const SubGridView = ({ restaurantItems, searchTerm, currentFilter }) => (
                 >
                   {x.title}
                 </a>
-              </h6>
+              </h4>
 
               {currentFilter !== "neighborhood" ? (
-                <span className="badge badge-pill badge-primary">
+                <span className="badge badge-pill badge-primary mr-2">
                   {x.neighborhood}
                 </span>
+              ) : (
+                <></>
+              )}
+              {x.cuisine === "" || x.cuisine === undefined ? (
+                <></>
               ) : (
                 <span className="badge badge-pill badge-secondary">
                   {x.cuisine}
                 </span>
               )}
 
-              <p>{x.service}</p>
-              {x.instagram === "" || x.instagram === undefined ? (
-                <></>
-              ) : (
-                <p>
-                  <i className="fab fa-instagram"></i>: {x.instagram}
-                </p>
-              )}
-              {x.phone === '' ||  x.phone === undefined ? (<></>):(
+              <p className="pt-2 pb-2">{x.service}</p>
               <p>
-                <i className="fa fa-phone"></i>:{x.phone}
-              </p>)}
-
+                {x.phone === "" || x.phone === undefined ? (
+                  <></>
+                ) : (
+                  <span>
+                    <i className="fa fa-phone mr-2"></i>{x.phone}
+                  </span>
+                )}
+                </p>
+                <p>
+                {x.instagram === "" || x.instagram === undefined ? (
+                  <></>
+                ) : (
+                  <span>
+                    <i className="fab fa-instagram mr-2"></i>
+                    <a href={`https://instagram.com/${x.instagram.slice(1)}`}>
+                      {x.instagram}
+                    </a>
+                  </span>
+                )}
+              </p>
 
               {/* <p>{x.merch}</p>
               <p>{x.giftcard}</p>
