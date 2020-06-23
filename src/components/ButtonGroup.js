36aeb9e27661fc98554f4809c246fce7bdcd0a93
Richard Saethang <richard.saethang@gmail.com>
2020-06-22 23:52:59 -0700

    Fixes responsive styles, removes dead code

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -7,14 +7,16 @@ const ButtonGroup = ({ toggleView }) => (
       className="btn btn-secondary"
       onClick={() => toggleView("list")}
     >
-      <i className="far fa-list mr-2"></i>{"  "}List
+      <i className="far fa-list"></i>
+      <span className="btn-text ml-2">List</span>
     </button>
     <button
       type="button"
       className="btn btn-secondary"
       onClick={() => toggleView("grid")}
     >
-      <i className="fa fa-th mr-2"></i> Grid
+      <i className="fa fa-th"></i>
+      <span className="btn-text ml-2">Grid</span>
     </button>
     {/* <button
                 type="button"
