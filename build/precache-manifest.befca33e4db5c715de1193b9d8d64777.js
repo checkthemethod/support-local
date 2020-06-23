7cfd238ce64897fe4d51f787f06de36797dffba6
Richard Saethang <richard.saethang@gmail.com>
2020-06-23 10:30:14 -0700

    Saves build folder assets

---
+++ build/precache-manifest.befca33e4db5c715de1193b9d8d64777.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6249406e04221f810a58abd11aa3e633",
+    "url": "/support-local/index.html"
+  },
+  {
+    "revision": "3b1976ac44e24ea130ef",
+    "url": "/support-local/static/css/2.cbdddf7c.chunk.css"
+  },
+  {
+    "revision": "f8efd044f791c43330d3",
+    "url": "/support-local/static/css/main.c3a2b263.chunk.css"
+  },
+  {
+    "revision": "3b1976ac44e24ea130ef",
+    "url": "/support-local/static/js/2.f96c6bef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/support-local/static/js/2.f96c6bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8efd044f791c43330d3",
+    "url": "/support-local/static/js/main.46e1f900.chunk.js"
+  },
+  {
+    "revision": "2846439fdba03f0979dd",
+    "url": "/support-local/static/js/runtime-main.93243040.js"
+  }
+]);
